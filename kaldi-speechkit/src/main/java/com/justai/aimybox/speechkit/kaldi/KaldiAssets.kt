19129fbffbb1985d78a1b485eca99fdc2a669942
Леонид Арефьев <mcsar@yandex.ru>
2022-06-09 22:30:11 +0200

    downloading from URL

--- kaldi-speechkit/src/main/java/com/justai/aimybox/speechkit/kaldi/KaldiAssets.kt
+++ kaldi-speechkit/src/main/java/com/justai/aimybox/speechkit/kaldi/KaldiAssets.kt
@@ -2,13 +2,51 @@ package com.justai.aimybox.speechkit.kaldi
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Build.VERSION.SDK_INT
 import java.io.File
+import java.io.FileOutputStream
 import java.io.InputStream
+import java.net.URL
+import java.nio.channels.Channels
 
 class KaldiAssets private constructor(
     val directory: String
-){
+) {
     companion object {
+
+
+        fun fromUrl(
+            context: Context,
+            lang: String,
+            force: Boolean
+        ): KaldiAssets {
+            var lang = "ru";
+            if (SDK_INT >= 24) {
+                lang = context.resources.configuration.locales.get(0).language.lowercase()
+            } else {
+                lang = context.resources.configuration.locale.language.lowercase()
+            }
+            val url = URL("http://wtf-dev.ru/kaldi_stt/model_${lang}.zip")
+            val outputFileName =
+                "${context.getExternalFilesDir(null)?.absolutePath}/kaldi-assets-${lang}/"
+            val dir = File(outputFileName + "ivector/")
+            if (!dir.exists()) {
+                val dir2 = File(outputFileName)
+                dir2.mkdirs()
+            } else {
+                if (!force) return KaldiAssets(outputFileName)
+            }
+            url.openStream().use {
+                Channels.newChannel(it).use { rbc ->
+                    FileOutputStream(outputFileName).use { fos ->
+                        fos.channel.transferFrom(rbc, 0, Long.MAX_VALUE)
+                    }
+                }
+            }
+            val zip = File(outputFileName + "model_${lang}.zip")
+            UnzipUtils.unzip(zip, outputFileName)
+            return KaldiAssets(outputFileName)
+        }
 
         @SuppressLint("NewApi")
         fun fromApkAssets(
@@ -33,13 +71,19 @@ class KaldiAssets private constructor(
             val assetManager = context.assets
             val files = assetManager.list(assetsDirectory)
 
-            files?.forEach {file ->
+            files?.forEach { file ->
                 try {
                     if (assetManager.list("$assetsDirectory/$file")!!.isNotEmpty()) {
-                        copyAssetToExternalStorage(context, "$assetsDirectory/$file", "$destination/$file")
+                        copyAssetToExternalStorage(
+                            context,
+                            "$assetsDirectory/$file",
+                            "$destination/$file"
+                        )
                     } else {
-                        copyAssetFileToExternalStorage(assetManager.open("$assetsDirectory/$file"),
-                            "$destination/$file")
+                        copyAssetFileToExternalStorage(
+                            assetManager.open("$assetsDirectory/$file"),
+                            "$destination/$file"
+                        )
                     }
                 } catch (e: Throwable) {
                     L.e("Cannot copy $destination/$file", e)
@@ -49,7 +93,8 @@ class KaldiAssets private constructor(
 
         private fun copyAssetFileToExternalStorage(
             stream: InputStream,
-            destinationPath: String) {
+            destinationPath: String
+        ) {
 
             stream.use { inputStream ->
                 File(destinationPath)
